4643ec69db9c131cdec172afe3420cc4acd8e331
Brad <bradleyrosenlof@gmail.com>
2022-05-18 11:22:33 -0500

    remove required providers

--- _versions.tf
+++ _versions.tf
@@ -5,33 +5,5 @@ terraform {
       source  = "vancluever/acme"
       version = "~> 2.8.0"
     }
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.52.0"
-    }
-    # template = {
-    #   source  = "hashicorp/template"
-    #   version = "~> 2.2.0"
-    # }
-    # local = {
-    #   source  = "hashicorp/local"
-    #   version = "~> 2.1.0"
-    # }
-    # null = {
-    #   source  = "hashicorp/null"
-    #   version = "~> 3.1.0"
-    # }
-    # archive = {
-    #   source  = "hashicorp/archive"
-    #   version = "~> 2.2.0"
-    # }
-    # random = {
-    #   source  = "hashicorp/random"
-    #   version = "~> 3.1.0"
-    # }
-    # time = {
-    #   source  = "hashicorp/time"
-    #   version = "~> 0.7.2"
-    # }
   }
 }
