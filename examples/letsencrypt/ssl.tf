7c1b7cfd734a838ec8aeac76c7b72ae219fdbde0
mani <mpanneerselvam@sigmasolve.net>
2023-04-12 23:59:07 +0530

    changes in letsencrypt ssl

--- examples/letsencrypt/ssl.tf
+++ examples/letsencrypt/ssl.tf
@@ -35,25 +35,26 @@ module "ssl_certificate_context" {
 module "ssl_certificate" {
   source  = "../.."
   context = module.ssl_certificate_context.self
-
-
-  additional_dns_names              = []
-  additional_secrets                = { EXAMPLE = "example value" }
-  create_mode                       = "LetsEncrypt"
-  create_secret_update_sns          = true
-  create_wildcard                   = true
-  import_filepath_certificate       = null
-  import_filepath_certificate_chain = null
-  import_filepath_private_key       = null
-  import_secret_arn                 = null
-  keyname_certificate               = "CERTIFICATE"
-  keyname_certificate_chain         = "CERTIFICATE_CHAIN"
-  keyname_private_key               = "CERTIFICATE_PRIVATE_KEY"
-  kms_key_deletion_window_in_days   = 7
-  kms_key_enable_key_rotation       = true
-  registration_email_address        = ""
-  secret_read_principals            = {}
-  secret_update_sns_pub_principals  = {}
-  secret_update_sns_sub_principals  = {}
-  zone_id                           = null
+  
+  additional_dns_names                = []
+  additional_secrets                  = { EXAMPLE = "example value" }
+  create_mode                         = "LetsEncrypt"
+  create_secret_update_sns            = true
+  create_wildcard                     = true
+  import_filepath_certificate         = null
+  import_filepath_certificate_chain   = null
+  import_filepath_csr                 = null
+  import_filepath_private_key         = null
+  import_secret_arn                   = null
+  keyname_certificate                 = "CERTIFICATE"
+  keyname_certificate_chain           = "CERTIFICATE_CHAIN"
+  keyname_certificate_signing_request = "CERTIFICATE_SIGNING_REQUEST"
+  keyname_private_key                 = "CERTIFICATE_PRIVATE_KEY"
+  kms_key_deletion_window_in_days     = 7
+  kms_key_enable_key_rotation         = true
+  registration_email_address          = ""
+  secret_read_principals              = {}
+  secret_update_sns_pub_principals    = {}
+  secret_update_sns_sub_principals    = {}
+  zone_id                             = null
 }
