7c604f44ceb88c9efcb822a8a4e56030418df3e9
mani <mpanneerselvam@sigmasolve.net>
2023-04-13 20:30:57 +0530

    change in secret source

--- secret.tf
+++ secret.tf
@@ -61,8 +61,9 @@ locals {
 # SSL Certificate SecretsManager Secret
 # --------------------------------------------------------------------------
 module "ssl_secret" {
-  source  = "registry.terraform.io/SevenPico/secret/aws"
-  version = "3.2.1"
+  #source  = "registry.terraform.io/SevenPico/secret/aws"
+  #version = "3.2.1"
+  source = "git::https://github.com/SevenPico/terraform-aws-secret.git?ref=hotfix/3.1.1"
   context = module.context.self
   enabled = module.context.enabled && local.create_secret
 
