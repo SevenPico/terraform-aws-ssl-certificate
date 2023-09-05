00b74d8760d2221c684fcc8275147bb8deb433da
Adarsh Hiwrale <ahiwrale@sigmasolve.net>
2023-09-06 01:29:43 +0530

    replace and certificate in us-east-1

--- examples/multi-region/ssl.tf
+++ examples/multi-region/ssl.tf
@@ -91,6 +91,42 @@ module "ssl_certificate" {
   zone_id                             = null
 }
 
+module "ssl_certificate_us-east-1" {
+  providers = {
+    aws = aws.us-east-1
+  }
+  source  = "../.."
+  context = module.ssl_certificate_context.self
+  depends_on = [
+    module.ssl_certificate,
+    aws_kms_replica_key.secondary
+  ]
+
+  replica_regions =  []
+  kms_key_id      = module.kms_key.key_id
+
+  save_csr                            = var.save_csr
+  additional_dns_names                = []
+  additional_secrets                  = { EXAMPLE = "example value" }
+  create_mode                         = "ImportSecret"
+  create_secret_update_sns            = true
+  create_wildcard                     = true
+  import_filepath_certificate         = null
+  import_filepath_certificate_chain   = null
+  import_filepath_csr                 = null
+  import_filepath_private_key         = null
+  import_secret_arn                   = replace(module.ssl_certificate.secret_arn, local.region , "us-east-1")
+  keyname_certificate                 = "CERTIFICATE"
+  keyname_certificate_chain           = "CERTIFICATE_CHAIN"
+  keyname_certificate_signing_request = "CERTIFICATE_SIGNING_REQUEST"
+  keyname_private_key                 = "CERTIFICATE_PRIVATE_KEY"
+  registration_email_address          = ""
+  secret_read_principals              = {}
+  secret_update_sns_pub_principals    = {}
+  secret_update_sns_sub_principals    = {}
+  zone_id                             = null
+}
+
 
 #------------------------------------------------------------------------------
 # Certbot Context
