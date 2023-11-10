c5f4393e8bd37f2fcd4051b31d9749b2110a2561
Adarsh Hiwrale <ahiwrale@sigmasolve.net>
2023-11-11 02:04:49 +0530

    subject alternative

--- letsencrypt.tf
+++ letsencrypt.tf
@@ -56,17 +56,20 @@ resource "tls_cert_request" "this" {
   dns_names       = var.create_wildcard ? ["*.${module.context.domain_name}"] : distinct(concat([module.context.dns_name], var.additional_dns_names))
 
   subject {
-    common_name = var.create_wildcard ? "*.${module.context.domain_name}" : module.context.domain_name
+    common_name = var.create_wildcard ? "*.${module.context.domain_name}" : module.context.dns_name
   }
 }
 
 resource "acme_certificate" "this" {
   count = module.letsencrypt_context.enabled ? 1 : 0
 
-  account_key_pem         = acme_registration.this[0].account_key_pem
-  certificate_request_pem = tls_cert_request.this[0].cert_request_pem
+  account_key_pem           = acme_registration.this[0].account_key_pem
+  certificate_request_pem   = tls_cert_request.this[0].cert_request_pem
+  common_name               = var.create_wildcard ? "*.${module.context.domain_name}" : module.context.dns_name
+  subject_alternative_names = distinct(concat([module.context.dns_name], var.additional_dns_names))
 
   dns_challenge {
     provider = "route53"
+
   }
 }
