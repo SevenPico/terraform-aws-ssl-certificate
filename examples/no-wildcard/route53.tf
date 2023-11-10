cf856bf3c336b553d9846755d5f5fe784cd5018a
Adarsh Hiwrale <ahiwrale@sigmasolve.net>
2023-11-11 00:56:25 +0530

    no wildcard

---
+++ examples/no-wildcard/route53.tf
@@ -0,0 +1,45 @@
+## ----------------------------------------------------------------------------
+##  Copyright 2023 SevenPico, Inc.
+##
+##  Licensed under the Apache License, Version 2.0 (the "License");
+##  you may not use this file except in compliance with the License.
+##  You may obtain a copy of the License at
+##
+##     http://www.apache.org/licenses/LICENSE-2.0
+##
+##  Unless required by applicable law or agreed to in writing, software
+##  distributed under the License is distributed on an "AS IS" BASIS,
+##  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+##  See the License for the specific language governing permissions and
+##  limitations under the License.
+## ----------------------------------------------------------------------------
+
+## ----------------------------------------------------------------------------
+##  ./examples/acm-only/route53.tf
+##  This file contains code written by SevenPico, Inc.
+## ----------------------------------------------------------------------------
+
+data "aws_route53_zone" "root" {
+  count = module.context.enabled ? 1 : 0
+  name  = var.root_domain
+
+}
+
+resource "aws_route53_zone" "public" {
+  #checkov:skip=CKV2_AWS_38:skip Domain Name System Security Extensions (DNSSEC) signing for Route 53 hosted zones
+  #checkov:skip=CKV2_AWS_39:skip (DNS) query logging for Route 53 hosted zones
+  count = module.context.enabled ? 1 : 0
+  name  = module.context.domain_name
+  tags  = module.context.tags
+}
+
+resource "aws_route53_record" "ns" {
+  count   = module.context.enabled ? 1 : 0
+  name    = module.context.id
+  type    = "NS"
+  zone_id = join("", data.aws_route53_zone.root[*].id)
+  records = length(aws_route53_zone.public) > 0 ? aws_route53_zone.public[0].name_servers : []
+  ttl     = 300
+}
+
+
