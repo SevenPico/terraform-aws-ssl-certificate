4c8fb5f72efd4d3ad2eabbc8cc885d6c815e50ec
Adarsh Hiwrale <ahiwrale@sigmasolve.net>
2023-11-10 19:57:01 +0530

    null

--- examples/letsencrypt-csr-only/ssl.tf
+++ examples/letsencrypt-csr-only/ssl.tf
@@ -47,6 +47,7 @@ module "ssl_certificate" {
   import_filepath_csr                 = null
   import_filepath_private_key         = null
   import_secret_arn                   = null
+  kms_key_id                          = null
   keyname_certificate                 = "CERTIFICATE"
   keyname_certificate_chain           = "CERTIFICATE_CHAIN"
   keyname_certificate_signing_request = "CERTIFICATE_SIGNING_REQUEST"
